2ad1d1daf01615dd0a0a2ebc192a1074ec9645d3
yelmouden <yassin.elmouden@gmail.com>
2024-03-16 14:27:10 +0100

    init project and init packages

---
+++ unsplah/unsplah/unsplahApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplahApp.swift
+//  Unsplah
+//
+//  Created by Yassin El Mouden on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
